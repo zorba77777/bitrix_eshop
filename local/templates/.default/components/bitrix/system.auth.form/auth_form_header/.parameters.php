1c3fa2c7de109285877c87a01d8b8c6324746c6e
Timur Sasin <student8@qsoft.ru>
2020-06-15 18:48:43 +0000

    138112 / Интегрирован блок Регистрация/авторизация в шапке сайта

---
+++ local/templates/.default/components/bitrix/system.auth.form/auth_form_header/.parameters.php
@@ -0,0 +1,9 @@
+<?php
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+
+$arTemplateParameters = array(
+    "PROFILE_PAGE" => array(
+        "NAME" => GetMessage("PROFILE"),
+        "TYPE" => "STRING",
+        "DEFAULT" => "/personal/profile/",)
+);
