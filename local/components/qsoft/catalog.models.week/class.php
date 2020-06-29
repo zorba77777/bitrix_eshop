c7463c7d22ee36badbfac35d403406b0f1364fee
Timur Sasin <student8@qsoft.ru>
2020-06-29 20:10:00 +0000

    138120 / Исправлены баги

--- local/components/qsoft/catalog.models.week/class.php
+++ local/components/qsoft/catalog.models.week/class.php
@@ -7,26 +7,31 @@ class ModelsWeek extends CBitrixComponent
 {
     public function onPrepareComponentParams($arParams)
     {
-        if (!isset($arParams["CACHE_TIME"]))
+        if (!isset($arParams["CACHE_TIME"])) {
             $arParams["CACHE_TIME"] = 3600;
+        }            
 
         $arParams["IBLOCK_TYPE"] = trim($arParams["IBLOCK_TYPE"]);
 
-        if (strlen($arParams["IBLOCK_TYPE"]) <= 0)
+        if (strlen($arParams["IBLOCK_TYPE"]) <= 0) {
             $arParams["IBLOCK_TYPE"] = "products";
+        }            
 
         $arParams["IBLOCK_ID"] = intval($arParams["IBLOCK_ID"]);
 
-        if ($arParams['IBLOCK_ID'] <= 0)
+        if ($arParams['IBLOCK_ID'] <= 0) {
             $arParams["IBLOCK_ID"] = "5";
+        }            
 
         $arParams["SORT_BY"] = trim($arParams["SORT_BY"]);
 
-        if (strlen($arParams["SORT_BY"]) <= 0)
+        if (strlen($arParams["SORT_BY"]) <= 0) {
             $arParams["SORT_BY"] = "RAND";
+        }            
 
-        if (!preg_match('/^(asc|desc|nulls)(,asc|,desc|,nulls){0,1}$/i', $arParams["SORT_ORDER"]))
+        if (!preg_match('/^(asc|desc|nulls)(,asc|,desc|,nulls){0,1}$/i', $arParams["SORT_ORDER"])) {
             $arParams["SORT_ORDER"] = "DESC";
+        }            
 
         $arParams["MODELS_COUNT"] = intval($arParams["MODELS_COUNT"]);
 
@@ -59,8 +64,8 @@ class ModelsWeek extends CBitrixComponent
                 ]));
             }
         }
-        
-       if ($this->startResultCache()) {
+
+        if ($this->startResultCache()) {
             $select = [
                 'ID',
                 'IBLOCK_ID',
@@ -75,7 +80,7 @@ class ModelsWeek extends CBitrixComponent
                 "IBLOCK_ID" => $this->arParams["IBLOCK_ID"],
                 "ACTIVE" => "Y",
                 "PROPERTY_MODEL_WEEKS_VALUE" => "Y",
-                ">CATALOG_PRICE_1" => 0 
+                ">CATALOG_PRICE_1" => 0
             );
 
             $navigation = ['nTopCount' => $this->arParams['MODELS_COUNT']];
@@ -89,8 +94,8 @@ class ModelsWeek extends CBitrixComponent
             );
 
             while ($item = $dbItem->GetNext()) {
-                
-                    $arButtons = CIBlock::GetPanelButtons(
+
+                $arButtons = CIBlock::GetPanelButtons(
                     $item["IBLOCK_ID"],
                     $item["ID"],
                     false,
@@ -106,13 +111,13 @@ class ModelsWeek extends CBitrixComponent
                 $item["DELETE_LINK_TEXT"] = $arButtons["edit"]["delete_element"]["TEXT"];
 
                 $arPreviewPicture[] = $item["PREVIEW_PICTURE"];
-                
+
                 $item['PRICES'] = CCatalogProduct::GetOptimalPrice($item["ID"]);
 
                 $item['CATALOG_QUANTITY'] = CCatalogProduct::GetByID($item["ID"])['QUANTITY'];
 
                 $item["ADD_URL"] = "?action=ADD2BASKET&id=" . $item["ID"];
-                
+
                 $this->arResult["ITEMS"][] = $item;
             }
 
